5f2214d73fab7bfd1a4ae60216f22a4912e64de8
ammar <ammar.ali.haidar.1990@gmail.com>
2024-06-03 21:46:10 +0400

    refine

--- app/(content)/dashboard/page.tsx
+++ app/(content)/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import Heading from "@/components/heading";
 import Image from "next/image";
 
+
 export default function Home() {
   return (
   <div className="">
-    <Heading title="Dahsbload" description="General statistics"/>
+    <Heading title="Dashboard" description="General statistics"/>
+
   </div>
   );
 }
