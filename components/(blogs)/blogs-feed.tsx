e031dffe43b826121c5728434aaa52214e06faf3
ammar <ammar.ali.haidar.1990@gmail.com>
2024-06-12 23:08:44 +0400

    refine

--- components/(blogs)/blogs-feed.tsx
+++ components/(blogs)/blogs-feed.tsx
@@ -13,7 +13,7 @@ const BlogsFeed = async (props: Props) => {
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 items-stretch ">
       <Link
         href={"/dashboard/blogs/new"}
-        className="flex flex-col relative items-center justify-center border rounded-md cursor-pointer overflow-hidden w-full group min-h-[300px] bg-white"
+        className="flex flex-col relative items-center justify-center border rounded-md cursor-pointer overflow-hidden w-full group min-h-[300px] bg-white shadow-md"
       >
         <div className="inset-0 gap-2 absolute w-full h-full bg-black opacity-0 group-hover:opacity-50 transition" />
         <h3 className="font-medium">Create new blog</h3>
