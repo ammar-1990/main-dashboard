afcf56aa969520102fa2e0e05622fd82ef2e523b
ammar <ammar.ali.haidar.1990@gmail.com>
2024-06-13 16:42:27 +0400

    overflow hidden blog card

--- components/(blogs)/blog-card.tsx
+++ components/(blogs)/blog-card.tsx
@@ -9,8 +9,8 @@ type Props = {
 const BlogCard = ({blog}: Props) => {
   return (
     <div className="border rounded-lg overflow-hidden shadow-md  h-full bg-white">
-    <div className="w-full aspect-video relative  ">
-      <div className="inset-0 absolute bg-black/60 z-10 overflow-hidden"/>
+    <div className="w-full aspect-video relative  overflow-hidden">
+      <div className="inset-0 absolute bg-black/60 z-10 "/>
       <Image src={blog.image} alt="image" fill className="object-contain z-10"/>
       <Image src={blog.image} alt="image" fill className="object-cover blur-md"/>
     </div>
